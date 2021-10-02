6bb9676ab420893cea841b647432f3c9a93b1283
Markus Zellner <markus@zellner-mail.de>
2021-10-02 14:15:22 +0200

    The tested file does not exist anymore with FE2 2.23 Using favicon.ico instead.

--- src/main/java/com/odin568/monitoring/software/FE2.java
+++ src/main/java/com/odin568/monitoring/software/FE2.java
@@ -14,10 +14,10 @@ public class FE2 implements Monitoring
     {
         var result = new ArrayList<MonitoringResult>();
 
-        result.add(HttpHelper.isSiteUpViaHttp("FE2 directly", "http://192.168.112.1:83/app.css", true));
-        result.add(HttpHelper.isSiteRedirectedToHttps("FE2 reverse proxy redirect", "http://192.168.112.200/app.css"));
-        result.add(HttpHelper.isSiteUpViaHttps("FE2 via reverse proxy", "https://192.168.112.200/app.css", true, true));
-        result.add(HttpHelper.isSiteUpViaHttps("FE2 via web", "https://haus.ffw-baudenbach.de/app.css", true, false));
+        result.add(HttpHelper.isSiteUpViaHttp("FE2 directly", "http://192.168.112.1:83/favicon.ico", true));
+        result.add(HttpHelper.isSiteRedirectedToHttps("FE2 reverse proxy redirect", "http://192.168.112.200/favicon.ico"));
+        result.add(HttpHelper.isSiteUpViaHttps("FE2 via reverse proxy", "https://192.168.112.200/favicon.ico", true, true));
+        result.add(HttpHelper.isSiteUpViaHttps("FE2 via web", "https://haus.ffw-baudenbach.de/favicon.ico", true, false));
 
         return result;
     }
